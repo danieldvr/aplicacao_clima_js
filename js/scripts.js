1f7d768d5c10aec484aba295aa3441ede424630a
danieldvr <daniell.vieira.rodrigues@gmail.com>
2024-07-17 18:17:22 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Seleção de elementos
+
+const apiKey = "1a84fedfaa6367eaeac218b02eddc550";
+const apiCountryURL = "https://flagsapi.com//shiny/64.png" 
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
